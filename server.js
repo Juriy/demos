7a622124de3aec38315410d21d702f2d21bfd349
Juriy Bura <juriy.bura@gmail.com>
2015-08-27 16:16:19 +0800

    After video 1 - handshake

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let http = require('http');
+let express = require('express');
+let socketio = require('socket.io');
+
+let app = express();
+let server = http.createServer(app);
+let io = socketio(server);
+
+io.on('connection', (sock) => sock.emit('msg', 'Hello!'));
+
+app.use(express.static(__dirname + '/client'));
+server.listen(8080, () => console.log('Ready to work!'));
